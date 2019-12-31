8d7d679bae14818265f95e01309969e64c88d592
Steven Conaway <steven.conaway@icloud.com>
2019-12-31 10:45:22 -0800

    Remove temporary fix; let Arduino CLI manage its directories

--- travis.sh
+++ travis.sh
@@ -15,16 +15,6 @@ board_manager:
         - https://dl.espressif.com/dl/package_esp32_index.json
 EOF
 
-if [[ "$(uname -s)" == "Darwin" ]]; then
-    mkdir -p /Users/travis/Library/Arduino15/packages
-    mkdir -p /Users/travis/Library/Arduino15/staging
-    mkdir -p /Users/travis/Library/Arduino15/tmp
-else
-    mkdir -p "$HOME/.arduino15/packages"
-    mkdir -p "$HOME/.arduino15/staging"
-    mkdir -p "$HOME/.arduino15/tmp"
-fi
-
 arduino-cli version
 
 arduino-cli config dump
